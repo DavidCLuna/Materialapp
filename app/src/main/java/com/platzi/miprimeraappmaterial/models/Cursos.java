043f7afb3f5fd3b12f79d624c682ddc77ab9d47a
Uriel Rodolfo Ramirez Moreno <topzihxc@gmail.com>
2016-03-06 00:13:27 -0600

    Agregando model and row

---
+++ app/src/main/java/com/platzi/miprimeraappmaterial/models/Cursos.java
@@ -0,0 +1,56 @@
+package com.platzi.miprimeraappmaterial.models;
+
+/**
+ * Created by Thespianartist on 05/03/16.
+ */
+public class Cursos {
+
+    Integer id;
+    String title;
+    String description;
+    String career;
+    String badge;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
+
+
+
+}
